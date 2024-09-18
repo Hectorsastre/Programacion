5d78f2991d916ad5b57d4d7c1e4679e32d203c0b
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-09-18 13:35:14 +0200

    Add files via upload

---
+++ Programacion/Pyton/Ejemplos/Prueba3.py
@@ -0,0 +1,8 @@
+var1=3
+var2="hola"
+#Preguntar algo que se responda por teclado es = input#
+var3=input("introduce el primer numero:")
+var4=input("introduce el segundo numero numero:")
+#int= transforma de texto a numero entero#
+var5=int (var3)+ int (var4)
+input("el resultado es",var5)
---
+++ Programacion/Pyton/Ejemplos/Prueba4.py
@@ -0,0 +1,6 @@
+#Preguntar algo que se responda por teclado es = input#
+var3=input("introduce el primer numero:")
+var4=input("introduce el segundo numero numero:")
+#int= transforma de texto a numero entero#
+var5=int (var3)+ int (var4)
+input("el resultado es",var5)

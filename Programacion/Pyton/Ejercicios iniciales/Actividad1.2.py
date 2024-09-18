667fcaf245f881301ff2bda1ba6ca07f3bf1c29f
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-09-18 13:33:07 +0200

    Add files via upload

---
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.2.py
@@ -0,0 +1,3 @@
+var1=int(input("Dime un numero entero"))
+var2=float(input("Dime un numero decimal"))
+var3=input("Dime una palabra")
---
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.4.py
@@ -0,0 +1,4 @@
+var1=float(input("Dame un numero: "))
+var2=float(input("Dame otro numero: "))
+var3=var1+var2
+print("El resultado de la suma de ",var1," y ",var2," es: ", var3)

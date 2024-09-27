5bf01fcaee1d75f684a59cba86ebb5123a07e355
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-09-27 12:43:55 +0200

    Add files via upload

---
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.12.py
@@ -0,0 +1,4 @@
+altura=float(input("Vamos a calcular el area y el perimetro de un trapecio isósceles, dame la altura: "))
+base_mayor=float(input("Dame la base mayor: "))
+base_menor=float(input("Dame la base menor: "))
+lado=float(input("Dame un numero: "))
--- Programacion/Pyton/Ejercicios iniciales/Actividad1.3.py
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.3.py
@@ -1,4 +1,4 @@
-var1=float(input("Dame un numero: "))
-var2=float(input("Dame otro numero: "))
+var1=int(input("Dame un numero entero: "))
+var2=int(input("Dame otro numero entero: "))
 var3=var1+var2
 print("El resultado de la suma de ",var1," y ",var2," es: ", var3)

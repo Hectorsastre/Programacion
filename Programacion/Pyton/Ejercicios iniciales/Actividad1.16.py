e31d0aadf234b6806939db283f9b9593c9f49450
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-09-28 21:23:28 +0200

    Create Actividad1.16.py

---
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.16.py
@@ -0,0 +1,5 @@
+import math
+var=float(input("Introduce un número: "))
+raiz_cuadrada=math.sqrt(var)
+division=int(raiz_cuadrada/2)
+print("La raíz cuadrada es",raiz_cuadrada," y la division entre 2 es",division)

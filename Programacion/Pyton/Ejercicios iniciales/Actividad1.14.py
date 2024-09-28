698c1c53d391a4248d2cc5f73b4d3aec0537ce42
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-09-28 21:07:16 +0200

    Actividad1.14.py

---
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.14.py
@@ -0,0 +1,8 @@
+import math
+diametro=float(input("Introduce el diámetro del círculo: "))
+radio=diametro/2
+area=math.pi*(radio**2)
+perimetro=2*math.pi*radio
+area=round(area,1)
+perimetro=round(perimetro,1)
+print("El perimetro seria de"perimetro" y el area sera de"area")

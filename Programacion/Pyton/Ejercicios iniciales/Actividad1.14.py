7534185307c0ad27161711187ac603086cff5b78
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-09-30 20:34:13 +0200

    Add files via upload

--- Programacion/Pyton/Ejercicios iniciales/Actividad1.14.py
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.14.py
@@ -5,4 +5,4 @@ area=math.pi*(radio**2)
 perimetro=2*math.pi*radio
 area=round(area,1)
 perimetro=round(perimetro,1)
-print("El perimetro seria de"perimetro" y el area sera de"area")
+print("El perimetro seria de",perimetro," y el area sera de",area)

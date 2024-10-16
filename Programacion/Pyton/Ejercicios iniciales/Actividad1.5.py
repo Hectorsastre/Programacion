3520f6e15f2acf58b91e60d2a438c2149c156a58
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-10-16 13:07:04 +0200

    Update Actividad1.5.py

--- Programacion/Pyton/Ejercicios iniciales/Actividad1.5.py
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.5.py
@@ -1,3 +1,4 @@
+#5. Programa que pida cinco palabras y muestre una frase con las cinco. Modifica el código para que entre palabra y palabra haya una coma.
 var1=input("Dame una palabra: ")
 var2=input("Dame otra palabra: ")
 var3=input("Dame otra palabra: ")

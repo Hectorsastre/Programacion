9cb6e4f0540f084b02f54cf68282b518b752da6d
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-10-16 13:21:49 +0200

    Update Actividad1.13.py

--- Programacion/Pyton/Ejercicios iniciales/Actividad1.13.py
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.13.py
@@ -1,4 +1,4 @@
-#13. Realiza un programa que, introduciendo en los valores de lado, base menor, base mayor y altura de un trapecio isósceles, nos devuelva por pantalla en el área y el perímetro.
+#13. Realiza un programa que, a partir introducir el lado de un cubo, presente por pantalla el área y para calcular el volumen utiliza el operador de exponente.
 lado=float(input("Introduce la longitud del lado del cubo: "))
 area=6*(lado**2)
 volumen=lado**3

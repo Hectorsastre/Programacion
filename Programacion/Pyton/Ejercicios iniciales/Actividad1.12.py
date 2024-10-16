9a166f4167f50e4633b67f5ccd8dbf1257be1ec0
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-10-16 13:20:04 +0200

    Update Actividad1.12.py

--- Programacion/Pyton/Ejercicios iniciales/Actividad1.12.py
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.12.py
@@ -1,3 +1,4 @@
+#12. Realiza un programa que, introduciendo en los valores de lado, base menor, base mayor y altura de un trapecio isósceles, nos devuelva por pantalla en el área y el perímetro.
 altura=float(input("Vamos a calcular el area y el perimetro de un trapecio isósceles, dame la altura: "))
 base_mayor=float(input("Dame la base mayor: "))
 base_menor=float(input("Dame la base menor: "))

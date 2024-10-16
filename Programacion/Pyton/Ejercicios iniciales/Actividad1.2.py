0e239586e4ab06ba0e8e6f808b0ba110d43a6d18
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-10-16 13:00:17 +0200

    Update Actividad1.2.py

--- Programacion/Pyton/Ejercicios iniciales/Actividad1.2.py
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.2.py
@@ -1,3 +1,5 @@
+#2. Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla
+en el siguiente orden: número entero, texto y número decimal.
 var1=int(input("Dime un numero entero"))
 var2=float(input("Dime un numero decimal"))
 var3=input("Dime una palabra")

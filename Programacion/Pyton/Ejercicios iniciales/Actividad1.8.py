00c5652967090351210df23a3a0f26e77a52e3a4
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-10-16 13:09:06 +0200

    Update Actividad1.8.py

--- Programacion/Pyton/Ejercicios iniciales/Actividad1.8.py
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.8.py
@@ -1,3 +1,4 @@
+#8. programa que pida un número de horas y muestre por pantalla los minutos y segundos
 horas=float(input("¿Cuantas horas quieres calcular? "))
 minutos=horas*60
 segundos=minutos*60

f47fb4705c656b31d11280717978aa493b333632
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-10-16 13:10:25 +0200

    Update Actividad1.10.py

--- Programacion/Pyton/Ejercicios iniciales/Actividad1.10.py
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.10.py
@@ -1,3 +1,4 @@
+#10. Introduce por teclado dos números y muestre por pantalla la siguiente información: cociente, resto y si el dividendo es par o impar.
 var1=float(input("Dame un numero: "))
 var2=float(input("Dame otro numero: "))
 cociente=var1//var2

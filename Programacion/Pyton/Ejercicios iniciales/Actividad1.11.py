fc621135571eb620d7f6a51e374fdd6d0a466bc2
Hectorsastre <hector.sastre@campus.xaloc.org>
2024-10-16 13:18:18 +0200

    Update Actividad1.11.py

--- Programacion/Pyton/Ejercicios iniciales/Actividad1.11.py
+++ Programacion/Pyton/Ejercicios iniciales/Actividad1.11.py
@@ -1,3 +1,4 @@
+#11. Realiza un programa que introduciendo el valor del lado de un cuadrado nos devuelva por pantalla en el área y el perímetro.
 var_lado=float(input("Dame la distancia de el lado de un cuadrado: "))
 perimetro=var_lado*4
 area=var_lado**2
